af19c656dbb31412b0e1daa23e2d8497e8c77763
chioma_nwanedo <chiomavanessa8@gmail.com>
2024-11-14 20:46:20 +0100

    latest update

--- jenkins_assignment.tf
+++ jenkins_assignment.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = "eu-west-2"  
 }
-resource "aws_s3_bucket" "veebucket" {
-  bucket = "veebucket"
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "jenkinsbucket"
 
   tags = {
     Name        = "My bucket"

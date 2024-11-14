688453a44153545a7ca077b13459223eea7046b5
chioma_nwanedo <chiomavanessa8@gmail.com>
2024-11-14 20:57:14 +0100

    help

--- jenkins_assignment.tf
+++ jenkins_assignment.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = "eu-west-2"  
 }
-resource "aws_s3_bucket" "jenkinsbucket" {
-  bucket = "jenkinsbucket"
+resource "aws_s3_bucket" "jenkins-bucket" {
+  bucket = "jenkins-bucket"
 
   tags = {
     Name        = "My bucket"

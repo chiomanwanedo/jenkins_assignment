803b14b541c1cb7b019ebb3cb0646b5c27c2fdb0
chioma_nwanedo <chiomavanessa8@gmail.com>
2024-11-14 20:26:51 +0100

    new update

--- jenkins_assignment.tf
+++ jenkins_assignment.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = "eu-west-2"  
 }
-resource "aws_s3_bucket" "cba_bucket" {
-  bucket = "terrabucket"
+resource "aws_s3_bucket" "jenkinsbucket" {
+  bucket = "jenkinsbucket"
 
   tags = {
     Name        = "My bucket"
